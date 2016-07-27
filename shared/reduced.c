52ae1f51e7c39478cf948adc438978a7aa1f0685
valentino <valentino.picotti@gmail.com>
2016-07-27 11:44:52 +0200

    Reduced version of shared

---
+++ shared/reduced.c
@@ -0,0 +1,188 @@
+//
+// Created by Valentino on 27/07/16.
+//
+
+#include <netdb.h> // EAI_MEMORY
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <rdma/fi_errno.h>
+#include "rdma/fi_eq.h"
+#include "shared/reduced.h"
+
+char default_port[8] = "9228";
+struct fi_info *fi, *hints;
+struct ft_opts opts;
+
+void ft_parseinfo(int op, char *optarg, struct fi_info *hints) {
+  switch (op) {
+  case 'n':
+    if (!hints->domain_attr) {
+      hints->domain_attr = malloc(sizeof *(hints->domain_attr));
+      if (!hints->domain_attr) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+      }
+    }
+    hints->domain_attr->name = strdup(optarg);
+    break;
+  case 'f':
+    if (!hints->fabric_attr) {
+      hints->fabric_attr = malloc(sizeof *(hints->fabric_attr));
+      if (!hints->fabric_attr) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+      }
+    }
+    hints->fabric_attr->prov_name = strdup(optarg);
+    break;
+  default:
+    /* let getopt handle unknown opts*/
+    break;
+
+  }
+}
+
+void ft_parse_addr_opts(int op, char *optarg, struct ft_opts *opts) {
+  switch (op) {
+  case 's':
+    opts->src_addr = optarg;
+    break;
+  case 'b':
+    opts->src_port = optarg;
+    break;
+  case 'p':
+    opts->dst_port = optarg;
+    break;
+  default:
+    /* let getopt handle unknown opts*/
+    break;
+  }
+}
+
+void ft_usage(char *name, char *desc) {
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "  %s [OPTIONS]\t\tstart server\n", name);
+  fprintf(stderr, "  %s [OPTIONS] <host>\tconnect to server\n", name);
+
+  if (desc)
+    fprintf(stderr, "\n%s\n", desc);
+
+  fprintf(stderr, "\nOptions:\n");
+  FT_PRINT_OPTS_USAGE("-n <domain>", "domain name");
+  FT_PRINT_OPTS_USAGE("-b <src_port>", "non default source port number");
+  FT_PRINT_OPTS_USAGE("-p <dst_port>", "non default destination port number");
+  FT_PRINT_OPTS_USAGE(
+      "-f <provider>",
+      "specific provider name eg sockets, verbs");
+  FT_PRINT_OPTS_USAGE("-s <address>", "source address");
+  FT_PRINT_OPTS_USAGE("-a <address vector name>", "name of address vector");
+  FT_PRINT_OPTS_USAGE("-h", "display this help output");
+
+  return;
+}
+
+static int dupaddr(
+    void **dst_addr,
+    size_t *dst_addrlen,
+    void *src_addr,
+    size_t src_addrlen) {
+  *dst_addr = malloc(src_addrlen);
+  if (!*dst_addr) {
+    FT_ERR("address allocation failed");
+    return EAI_MEMORY;
+  }
+  *dst_addrlen = src_addrlen;
+  memcpy(*dst_addr, src_addr, src_addrlen);
+  return 0;
+}
+
+static int getaddr(
+    char *node,
+    char *service,
+    struct fi_info *hints,
+    uint64_t flags) {
+  int ret;
+  struct fi_info *fi;
+
+  if (!node && !service) {
+    if (flags & FI_SOURCE) {
+      hints->src_addr = NULL;
+      hints->src_addrlen = 0;
+    } else {
+      hints->dest_addr = NULL;
+      hints->dest_addrlen = 0;
+    }
+    return 0;
+  }
+
+  ret = fi_getinfo(FT_FIVERSION, node, service, flags, hints, &fi);
+  if (ret) {
+    FT_PRINTERR("fi_getinfo", ret);
+    return ret;
+  }
+  hints->addr_format = fi->addr_format;
+
+  if (flags & FI_SOURCE) {
+    ret = dupaddr(
+        &hints->src_addr,
+        &hints->src_addrlen,
+        fi->src_addr,
+        fi->src_addrlen);
+  } else {
+    ret = dupaddr(
+        &hints->dest_addr,
+        &hints->dest_addrlen,
+        fi->dest_addr,
+        fi->dest_addrlen);
+  }
+
+  fi_freeinfo(fi);
+  return ret;
+}
+
+int ft_getsrcaddr(char *node, char *service, struct fi_info *hints) {
+  return getaddr(node, service, hints, FI_SOURCE);
+}
+
+int ft_read_addr_opts(
+    char **node,
+    char **service,
+    struct fi_info *hints,
+    uint64_t *flags,
+    struct ft_opts *opts) {
+  int ret;
+
+  if (opts->dst_addr) {
+    if (!opts->dst_port)
+      opts->dst_port = default_port;
+
+    ret = ft_getsrcaddr(opts->src_addr, opts->src_port, hints);
+    if (ret)
+      return ret;
+    *node = opts->dst_addr;
+    *service = opts->dst_port;
+  } else {
+    if (!opts->src_port)
+      opts->src_port = default_port;
+
+    *node = opts->src_addr;
+    *service = opts->src_port;
+    *flags = FI_SOURCE;
+  }
+
+  return 0;
+}
+
+void eq_readerr(struct fid_eq *eq, const char *eq_str) {
+  struct fi_eq_err_entry eq_err;
+  int rd;
+
+  rd = fi_eq_readerr(eq, &eq_err, 0);
+  if (rd != sizeof(eq_err)) {
+    FT_PRINTERR("fi_eq_readerr", rd);
+  } else {
+    FT_EQ_ERR(eq, eq_err, NULL, 0);
+  }
+}
---
+++ shared/reduced.h
@@ -0,0 +1,125 @@
+//
+// Created by Valentino on 27/07/16.
+// Minimal version of shared.h required by rc_pingpong.h
+//
+
+#ifndef FI_TEST_REDUCED_H
+#define FI_TEST_REDUCED_H
+
+#include <inttypes.h>
+
+#include "rdma/fabric.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef FT_FIVERSION
+#define FT_FIVERSION FI_VERSION(1,3)
+#endif
+
+#define FT_DEFAULT_SIZE    (1 << 0)
+
+extern char default_port[8];
+
+enum ft_comp_method {
+  FT_COMP_SPIN = 0,
+  FT_COMP_SREAD,
+  FT_COMP_WAITSET,
+  FT_COMP_WAIT_FD
+};
+
+enum {
+  FT_OPT_ACTIVE = 1 << 0,
+  FT_OPT_ITER = 1 << 1,
+  FT_OPT_SIZE = 1 << 2,
+  FT_OPT_RX_CQ = 1 << 3,
+  FT_OPT_TX_CQ = 1 << 4,
+  FT_OPT_RX_CNTR = 1 << 5,
+  FT_OPT_TX_CNTR = 1 << 6,
+  FT_OPT_VERIFY_DATA = 1 << 7,
+  FT_OPT_ALIGN = 1 << 8,
+};
+
+struct ft_opts {
+  int iterations;
+  int warmup_iterations;
+  int transfer_size;
+  int window_size;
+  char *src_port;
+  char *dst_port;
+  char *src_addr;
+  char *dst_addr;
+  char *av_name;
+  int sizes_enabled;
+  int options;
+  enum ft_comp_method comp_method;
+  int machr;
+  int argc;
+  char **argv;
+};
+
+extern struct fi_info *fi, *hints;
+
+
+extern struct ft_opts opts;
+
+void ft_parseinfo(int op, char *optarg, struct fi_info *hints);
+void ft_parse_addr_opts(int op, char *optarg, struct ft_opts *opts);
+void ft_usage(char *name, char *desc);
+
+#define ADDR_OPTS "b:p:s:a:"
+#define INFO_OPTS "n:f:"
+
+#define INIT_OPTS (struct ft_opts) \
+  {  .options = FT_OPT_RX_CQ | FT_OPT_TX_CQ, \
+    .iterations = 1000, \
+    .warmup_iterations = 10, \
+    .transfer_size = 1024, \
+    .window_size = 64, \
+    .sizes_enabled = FT_DEFAULT_SIZE, \
+    .argc = argc, .argv = argv \
+  }
+
+int ft_read_addr_opts(
+    char **node,
+    char **service,
+    struct fi_info *hints,
+    uint64_t *flags,
+    struct ft_opts *opts);
+
+#define FT_PRINTERR(call, retv) \
+  do { fprintf(stderr, call "(): %s:%d, ret=%d (%s)\n", __FILE__, __LINE__, \
+      (int) retv, fi_strerror((int) -retv)); } while (0)
+
+#define FT_LOG(level, fmt, ...) \
+  do { fprintf(stderr, "[%s] fabtests:%s:%d: " fmt "\n", level, __FILE__, \
+      __LINE__, ##__VA_ARGS__); } while (0)
+
+#define FT_ERR(fmt, ...) FT_LOG("error", fmt, ##__VA_ARGS__)
+
+#define FT_EQ_ERR(eq, entry, buf, len) \
+  FT_ERR("eq_readerr: %s", fi_eq_strerror(eq, entry.prov_errno, \
+        entry.err_data, buf, len))
+
+
+
+
+void eq_readerr(struct fid_eq *eq, const char *eq_str);
+
+#define FT_PROCESS_QUEUE_ERR(readerr, rd, queue, fn, str)  \
+  do {              \
+    if (rd == -FI_EAVAIL) {        \
+      readerr(queue, fn " " str);    \
+    } else {          \
+      FT_PRINTERR(fn, rd);      \
+    }            \
+  } while (0)
+
+#define FT_PROCESS_EQ_ERR(rd, eq, fn, str) \
+  FT_PROCESS_QUEUE_ERR(eq_readerr, rd, eq, fn, str)
+
+#define FT_PRINT_OPTS_USAGE(opt, desc) fprintf(stderr, " %-20s %s\n", opt, desc)
+
+#endif //FI_TEST_REDUCED_H
