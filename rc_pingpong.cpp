75d0d33d750ecfab80b3bdd39cb12ee14d33c84a
Matteo Manzalli <matteo.manzali@gmail.com>
2016-07-29 15:29:47 +0200

    Porting the C code to C++

--- rc_pingpong.cpp
+++ rc_pingpong.cpp
@@ -1,45 +1,3 @@
-/*
- * Copyright (c) 2005 Topspin Communications.  All rights reserved.
- * Copyright (c) 2014 Intel Corp.  All rights reserved.
- *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * BSD license below:
- *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/*
- * This is a port of libiverbs/examples/rc_pingong.c to libfabric.
- * It's a simple pingong test with connected endpoints.
- */
-
-#if HAVE_CONFIG_H
-#  include <config.h>
-#endif /* HAVE_CONFIG_H */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -56,7 +14,7 @@
 #include <rdma/fi_cm.h>
 #include <rdma/fi_errno.h>
 
-#include "shared/reduced.h"
+#include "shared/reduced.hpp"
 
 #define FT_CLOSE(DESC, STR)         \
   do {            \
@@ -364,7 +322,7 @@ static struct pingpong_context *pp_init_ctx(
   struct pingpong_context *ctx;
   int rc = 0;
 
-  ctx = calloc(1, sizeof *ctx);
+  ctx = static_cast<pingpong_context*>(calloc(1, sizeof *ctx));
   if (!ctx)
     return NULL;
 
